44301e9a79fee1aa2fff7f341a08868af6dc1615
Adeiro <81812137+Adeiro@users.noreply.github.com>
2021-11-06 01:00:59 +0300

    Не работающий вариант с массивом ссылок на соседей

--- Life00/Assets/Script/GameManager.cs
+++ Life00/Assets/Script/GameManager.cs
@@ -1,15 +1,49 @@
-using System;
+using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
+// Этот класс будет отвечать за создание, уничтожение и жизнь всех клеточек
+
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject pfsquare;
+    [SerializeField] private GameObject pfsquare = null;
     [SerializeField] private float delaytime = 0;
+    [SerializeField] private bool work = false;
+
+    private List<Vector2Int> newadd;  // Те, кто будут добавлены
+    private List<ScrKillSquare> newdel;  // Те, кто будут удалены
+
+// Создаем клетку по определенным координатам (в дальнейшем - с проверкой целостности и добавлением в списки)
+    public void addCell(Vector2Int CreatePos)
+    {  
+        GameObject newgo = Instantiate(pfsquare);
+        ScrKillSquare kkk = newgo.GetComponent<ScrKillSquare>();
+        kkk.BornMe(transform,CreatePos);
+    }
+
+// Удаляем определенную клетку с доски (с поддержкой целостности)
+    public void delCell(GameObject cell)
+    {
+        cell.GetComponent<ScrKillSquare>().KillMe();
+    }
+
+    // Реакция на действия пользователя
+    private void OnMouseDown()
+    {
+        
+        Vector3 newpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2Int crpos = new Vector2Int(Mathf.RoundToInt(newpos.x), Mathf.RoundToInt(newpos.y));
+        addCell(crpos);
+    }
+
 
     void Start()
     {
+        newadd = new List<Vector2Int>();
+        newdel = new List<ScrKillSquare>();
+        if (pfsquare == null) pfsquare = null;
         if (delaytime == 0) delaytime = 10;
+        work = false;
         StartCoroutine("OneStep");
     }
 
@@ -18,43 +52,44 @@ public class GameManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(delaytime);
-            // Выполняем работу для каждого квадратика среди имеющихся
-            GameObject[] squares = GameObject.FindGameObjectsWithTag("Live");
-            foreach (GameObject square in squares)
+            if (work)
             {
-                WorkProc(square);
+                // Выполняем работу для каждого квадратика среди имеющихся
+                GameObject[] squares = GameObject.FindGameObjectsWithTag("Live");
+                newadd.Clear();
+                newdel.Clear();
+                foreach (GameObject square in squares)
+                {
+                    ScrKillSquare mysq = square.GetComponent<ScrKillSquare>();
+                    List<Vector2Int> checksq = mysq.GetNextCellsCount();
+                    int NeightNum = 8 - checksq.Count;
+                    if ((NeightNum < 2) || (NeightNum > 3))
+                    {
+                        newdel.Add(mysq);
+                    }
+                    foreach (Vector2Int chk in checksq)
+                    {
+                        if (!newadd.Contains(chk)) newadd.Add(chk);
+                    }
+                }
+                // Добавляем новеньких (если им повезло)
+                foreach (Vector2Int curpos in newadd)
+                {
+                    addCell(curpos);
+                }
+                // Убиваем стареньких (которым не повезло)
+                foreach (ScrKillSquare pos in newdel)
+                {
+                    pos.KillMe();
+                }
             }
         }
     }
 
-    private void CheckAndCreate(Vector2 cpos)
-    {
-        // Проверяем наличие квадратиков по новым позициям
-        Vector3 stp = new Vector3(cpos.x, cpos.y, 1);
-        Vector3 etp = new Vector3(0,0,-1);
-        if (!Physics.Raycast(stp, etp, 2,0xFFFF)) {
-            Vector3 curpos = new Vector3(cpos.x, cpos.y, 0);
-            GameObject newgo = Instantiate(pfsquare);
-            newgo.transform.position = curpos;
-        }
-    }
-
-    private void WorkProc(GameObject square)
-    {
-        Vector2 curpos = square.transform.position;
-        CheckAndCreate(new Vector2(curpos.x - 1, curpos.y    ));
-        CheckAndCreate(new Vector2(curpos.x + 1, curpos.y    ));
-        CheckAndCreate(new Vector2(curpos.x    , curpos.y - 1));
-        CheckAndCreate(new Vector2(curpos.x    , curpos.y + 1));
-        CheckAndCreate(new Vector2(curpos.x - 1, curpos.y - 1));
-        CheckAndCreate(new Vector2(curpos.x + 1, curpos.y + 1));
-        CheckAndCreate(new Vector2(curpos.x + 1, curpos.y - 1));
-        CheckAndCreate(new Vector2(curpos.x - 1, curpos.y + 1));
-    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
--- Life00/Assets/Script/ScrKillSquare.cs
+++ Life00/Assets/Script/ScrKillSquare.cs
@@ -4,9 +4,54 @@ using UnityEngine;
 
 public class ScrKillSquare : MonoBehaviour
 {
+    private GameObject[] sq;
+    private Vector2Int mypos;
+
+    private void Start()
+    {
+        for (int i = 0; i < 9; i++) sq[i] = null; // 5 элемент - мо€ клетка - будет всегда nil чтобы не сношать мозг преобразованием координат
+    }
     private void OnMouseDown()
     {
+        KillMe();
+    }
+
+    // –ождение новой клеточки в нужных координатах
+    public void BornMe(Transform parent, Vector2Int newpos)
+    {
+        Vector3 np = new Vector3(newpos.x, newpos.y, 0);
+        mypos = newpos;
+        // —канируем объекты вокруг клетки
+        Vector2 dirv = new Vector2((float)0.25, 0);
+        sq[0] = Physics2D.Raycast(new Vector2(newpos.x - 1, newpos.y + 1), dirv, (float)0.25, 0xFFFF, (float)-0.5, (float)0.5).collider.gameObject;
+        sq[1] = Physics2D.Raycast(new Vector2(newpos.x, newpos.y + 1), dirv, (float)0.25, 0xFFFF, (float)-0.5, (float)0.5).collider.gameObject;
+        sq[2] = Physics2D.Raycast(new Vector2(newpos.x + 1, newpos.y + 1), dirv, (float)0.25, 0xFFFF, (float)-0.5, (float)0.5).collider.gameObject;
+        sq[3] = Physics2D.Raycast(new Vector2(newpos.x - 1, newpos.y), dirv, (float)0.25, 0xFFFF, (float)-0.5, (float)0.5).collider.gameObject;
+        sq[4] = null;
+        sq[5] = Physics2D.Raycast(new Vector2(newpos.x + 1, newpos.y), dirv, (float)0.25, 0xFFFF, (float)-0.5, (float)0.5).collider.gameObject;
+        sq[6] = Physics2D.Raycast(new Vector2(newpos.x - 1, newpos.y - 1), dirv, (float)0.25, 0xFFFF, (float)-0.5, (float)0.5).collider.gameObject;
+        sq[7] = Physics2D.Raycast(new Vector2(newpos.x, newpos.y - 1), dirv, (float)0.25, 0xFFFF, (float)-0.5, (float)0.5).collider.gameObject;
+        sq[8] = Physics2D.Raycast(new Vector2(newpos.x + 1, newpos.y - 1), dirv, (float)0.25, 0xFFFF, (float)-0.5, (float)0.5).collider.gameObject;
+        gameObject.transform.parent = parent;
+        gameObject.transform.position = np;
+    }
+
+    public void KillMe()
+    {
+        // Ѕудем наде€тьс€ что при удалении автоматом удал€тс€ и ссылки на эту клетку у всех соседей
         Destroy(gameObject);
     }
+
+    // ¬озвращает список пустых клеточек вокруг текущей клетки
+    public List<Vector2Int> GetNextCellsCount()
+    {
+        List<Vector2Int> newadd = new List<Vector2Int>();
+        for (int n = 0; n < 9; n++) if (n != 4)
+            {
+                if (sq[n] == null) newadd.Add(new Vector2Int(mypos.x + (((n == 0) || (n == 3) || (n == 6)) ? -1 : (((n == 2) || (n == 5) || (n == 8)) ? 1 : 0)), mypos.y + ((n < 3) ? 1 : ((n > 5) ? 1 : 0))));
+            }
+        return newadd;
+    }
+
 
 }
